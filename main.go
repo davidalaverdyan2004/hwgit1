bc33b6cbdb1701619fc8246b41731fa110565e77
David Alaverdyan <arkadiialaverdyan@mail.ru>
2026-02-26 20:45:09 +0300

    add simple connection

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	feature "study/feature1"
+	"study/feature_postgres/simple_connection"
 )
 
 func main() {
 	fmt.Println("githw1 !!!")
 
 	feature.Feature1("feature1 !!!")
+
+	simple_connection.CheckConnection()
 }
